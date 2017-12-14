1bb9fe0dc9ace6a63c0a92a1415a25992342c623
TamasKerdo <kerdothamas@gmail.com>
2017-12-14 11:50:28 +0100

    feat(ToDoList):FileHandler class is done but not tested

---
+++ TodoList/ToDoList/FileHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ToDoList
+{
+    class FileHandler
+    {
+        public String FileName { get; set; }
+        public String commandName { get; set; }
+        public List<string> textContent { get; set; }
+
+        FileHandler(string FileName, string commandName)
+        {
+            this.FileName = FileName;
+            this.commandName = commandName;
+            textContent = new List<string>();
+
+        }
+
+        public void ReadToDoFile()
+        {
+            StreamReader sr = new StreamReader(FileName);
+            string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                textContent.Add(line);
+                }            
+        }
+
+        public void WriteToDoFile()
+        {
+            TextWriter tw = new StreamWriter(FileName);
+
+            for (int i = 0; i < textContent.Count; i++)
+            {
+                tw.WriteLine(textContent[i],false);
+            }
+
+            tw.Close();
+        }
+
+    }
+}
