f9c9b748ca923a3aceef7607890f10c43aeb6db6
TamasKerdo <kerdothamas@gmail.com>
2017-12-14 12:27:13 +0100

    feat(ToDoList):add new task class is done

---
+++ TodoList/ToDoList/AddTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList 
+{
+    class AddTask : FileHandler
+    {
+        public string InputTask { get; set; }
+
+        public AddTask(string FileName, string InputTask) : base(FileName)
+        {
+            this.InputTask = InputTask;
+        }
+
+        public void AddTaskToFile()
+        {
+            ReadToDoFile();
+            textContent.Add(InputTask);
+            WriteToDoFile();
+        }
+    }
+}
